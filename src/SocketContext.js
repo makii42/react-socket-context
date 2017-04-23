57bb886b9554340a601f7f403be5515d552417c5
Ralf Fischer <makii@meet-the-geek.net>
2017-04-22 22:48:53 -0700

    upgrades to new prop-types pkg

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -1,4 +1,5 @@
-import { Component, PropTypes } from 'react';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 import SocketIOClient from 'socket.io-client';
 
 class SocketContext extends Component {
